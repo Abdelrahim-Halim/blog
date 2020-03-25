cd3a3d8dd45d4239173f5139c53e9d1d5a74b210
halim <halim@192.168.1.5>
2020-03-25 15:58:04 +0200

    update

--- rails-project/db/migrate/20200325104436_create_posts.rb
+++ rails-project/db/migrate/20200325104436_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.text :body
+      t.string :title, null: false, unique: true
+      t.text :body, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
